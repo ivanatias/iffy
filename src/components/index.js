5a2ed90827b929c1da9896f5c2d58dd67dbc9daf
ivanatias <ivan.d.atias@gmail.com>
2022-07-20 17:42:16 -0400

    export components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from './Header'
+export { default as Search } from './Search'
+export { default as Footer } from './Footer'
+export { default as Section } from './Section'
+export { default as Container } from './Container'
+export { default as Layout } from './Layout'

37c0dccff9052ec55c1c4e6c7ff2adc066eea8e7
ivanatias <ivan.d.atias@gmail.com>
2022-07-21 18:08:47 -0400

    add stickers and gifs adapters

---
+++ src/adapters/adapter.gifs.js
@@ -0,0 +1,12 @@
+export const gifsAdapter = (gifsData) => {
+  return gifsData.map(({ id, images, slug, title, user, username }) => ({
+    id,
+    url: images.fixed_height.url,
+    width: images.fixed_height.width,
+    height: images.fixed_height.height,
+    slug,
+    title,
+    username: username || 'unknown user',
+    userAvatar: user?.avatar_url,
+  }))
+}

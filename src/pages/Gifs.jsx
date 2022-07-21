793ea408d84cbae706beb8bc08ad6cd5d12e5dcf
ivanatias <ivan.d.atias@gmail.com>
2022-07-21 18:11:01 -0400

    create Gifs and Stickers pages and implement data fetching

--- src/pages/Gifs.jsx
+++ src/pages/Gifs.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Grid } from '../components'
+import { useFetchAndLoad } from '../hooks/useFetchAndLoad'
+import { useAsync } from '../hooks/useAsync'
+import { getGifs } from '../services/public.service.gifs'
+import { gifsAdapter } from '../adapters/adapter.gifs'
 
 const Gifs = () => {
-  return <div>Gifs</div>
+  const [gifs, setGifs] = useState([])
+  const { callEndpoint, loading, error } = useFetchAndLoad()
+
+  const getApiData = async () => await callEndpoint(getGifs())
+
+  const adaptGifs = (results) => {
+    setGifs(gifsAdapter(results.data))
+  }
+
+  useAsync(getApiData, adaptGifs)
+
+  if (loading) return <div>Loading...</div>
+
+  if (error) return <div>{error.message}</div>
+
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error.message}</div>
+      ) : gifs.length ? (
+        <Grid items={gifs} />
+      ) : (
+        <div>No results found</div>
+      )}
+    </>
+  )
 }
 
 export default Gifs
--- src/pages/Stickers.jsx
+++ src/pages/Stickers.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Grid } from '../components'
+import { useFetchAndLoad } from '../hooks/useFetchAndLoad'
+import { useAsync } from '../hooks/useAsync'
+import { getStickers } from '../services/public.service.stickers'
+import { stickersAdapter } from '../adapters/adapter.stickers'
 
 const Stickers = () => {
-  return <div>Stickers</div>
+  const [stickers, setStickers] = useState([])
+  const { callEndpoint, loading, error } = useFetchAndLoad()
+
+  const getApiData = async () => await callEndpoint(getStickers())
+
+  const adaptStickers = (results) => {
+    setStickers(stickersAdapter(results.data))
+  }
+
+  useAsync(getApiData, adaptStickers)
+
+  if (loading) return <div>Loading...</div>
+
+  if (error) return <div>{error.message}</div>
+
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error.message}</div>
+      ) : stickers.length ? (
+        <Grid items={stickers} />
+      ) : (
+        <div>No results found</div>
+      )}
+    </>
+  )
 }
 
 export default Stickers

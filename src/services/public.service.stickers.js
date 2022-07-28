0348bf0700d47a0de8583d8ccf42930c5fc2a2e7
ivanatias <ivan.d.atias@gmail.com>
2022-07-27 22:40:39 -0400

    modify public services to handle pagination

--- src/services/public.service.stickers.js
+++ src/services/public.service.stickers.js
@@ -3,11 +3,16 @@ import { loadAbort } from '../utilities/load-abort.utility'
 const BASE_URL = 'https://api.giphy.com/v1/stickers'
 const API_KEY = import.meta.env.VITE_GIPHY_API_KEY
 
-export const getStickers = (searchTerm) => {
+export const getStickers = (searchTerm, page) => {
   const controller = loadAbort()
+  const limit = 20
   const endpoint = searchTerm
-    ? `${BASE_URL}/search?api_key=${API_KEY}&q=${searchTerm}`
-    : `${BASE_URL}/trending?api_key=${API_KEY}&limit=20`
+    ? `${BASE_URL}/search?api_key=${API_KEY}&q=${searchTerm}&limit=${limit}&offset=${
+        page * limit
+      }`
+    : `${BASE_URL}/trending?api_key=${API_KEY}&limit=${limit}&offset=${
+        page * limit
+      }`
   return {
     call: fetch(endpoint, { signal: controller.signal }),
     controller,

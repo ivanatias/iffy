265f9b0714d8b9c2d4ccd0804c9475152d098dfe
ivanatias <ivan.d.atias@gmail.com>
2022-07-27 22:47:30 -0400

    modify App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import { Layout } from './components'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { Layout, LoadScreen } from './components'
 import { Gifs, Stickers } from './pages'
+import { useSearch } from './hooks/useSearch'
 
 const App = () => {
+  const { searchTerm } = useSearch()
+  const { pathname } = useLocation()
+
+  if (pathname === '/') return <LoadScreen />
+
   return (
     <Layout>
       <Routes>
-        <Route path='/gifs' element={<Gifs />} />
-        <Route path='/stickers' element={<Stickers />} />
+        <Route
+          path='/gifs'
+          element={<Gifs key={searchTerm} searchTerm={searchTerm} />}
+        />
+        <Route
+          path='/stickers'
+          element={<Stickers key={searchTerm} searchTerm={searchTerm} />}
+        />
       </Routes>
     </Layout>
   )
